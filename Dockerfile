d314c4164246f5b49f93592a4605daeda2585a11
Zachery Zhu <46687719+Zachery2008@users.noreply.github.com>
2020-01-28 22:26:40 -0800

    asd

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,13 @@ FROM node:13
 WORKDIR /usr/src/multi-repo1
 #
 COPY package*.json ./
+#
 RUN npm install
 #
 COPY . .
+#
+ENV PORT=3000
+#
+EXPOSE ${PORT}
 #
 CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World from multi-repo1!');
+  res.send('Hello World from multi-repo1!')
 });
 
 // use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
+var port = process.env.port || 3000
 
-app.listen(port);
+app.listen(port, () => console.log(`Example app listening on port ${port} !`))
