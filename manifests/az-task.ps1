a96aee16e50e3b86c3df0df958cfda173d9b3fe1
Zachery2008 <46687719+Zachery2008@users.noreply.github.com>
2020-01-31 11:43:55 -0800

    Update az-task.ps1

--- manifests/az-task.ps1
+++ manifests/az-task.ps1
@@ -1,3 +1,3 @@
-az login --service-principal -u zach.zhu@lifebooster.ca -p Unsw85474?
+az login -u zach.zhu@lifebooster.ca -p Unsw85474?
 
 az aks update -n senz-aks-test -g rg-senz-aks-usce-dev --attach-acr senzakstest
