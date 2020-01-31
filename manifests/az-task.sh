68f4b84a1f79f7b401ff159eed24e7222407578b
Zachery2008 <46687719+Zachery2008@users.noreply.github.com>
2020-01-31 13:37:27 -0800

    update

--- manifests/az-task.sh
+++ manifests/az-task.sh
@@ -1,3 +1,6 @@
 az login -u zach.zhu@lifebooster.ca -p Unsw85474?
 
+# Currently, need to enable preview to process ACR integration. 2020-01-31 
+az extension update --name aks-preview
+
 az aks update -n senz-aks-test -g rg-senz-aks-usce-dev --attach-acr senzakstest
