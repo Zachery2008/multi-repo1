118b63c336f7a8d0f712d5ed32d6f03faeef184c
Zachery2008 <zachery.zhu@lifebooster.ca>
2020-09-16 15:01:35 -0700

    bull-queue

--- packages/bull-queue-metrics/src/index.ts
+++ packages/bull-queue-metrics/src/index.ts
@@ -1,21 +1,60 @@
 import Queue from 'bull'
+import express = require('express')
 import promClient from 'prom-client'
-import * as bullProm from 'bull-prom';
- 
-let queue = new Queue('testQueue', {
+import client = require('prom-client')
+import { bullQueueMetrics } from 'src/lib/bullQueueMetrics'
+import { captureQueueName, assessmentQueueName, capturePrefix, assessmentPrefix} from 'src/constants'
+const delay = require('delay')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+const app: express.Application = express()
+/*
+const captureQueue = new Queue(captureQueueName, {
   redis: {
     port: parseInt(process.env.REDIS_PORT || '6379'),
-    host: process.env.REDIS_HOST
-  },
+    host: process.env.REDIS_HOST,
+  }
 })
- 
-const bullMetric = bullProm.init({
+
+const assessmentQueue = new Queue(assessmentQueueName, {
+  redis: {
+    port: parseInt(process.env.REDIS_PORT || '6379'),
+    host: process.env.REDIS_HOST,
+  }
+})
+
+// create captureQueue metrics client
+const captureMetric = new bullQueueMetrics({
   promClient, // optional, it will use internal prom client if it is not given
-  interval: 1000, // optional, in ms, default to 60000
-});
- 
-const started = bullMetric.start(queue)
- 
-// Optional
-//started.stop()
- 
+  interval: 5000, // optional, in ms, default to 60000
+}, captureQueueName, capturePrefix)
+
+// create assessmentQueue metrics client
+const assessmentMetric = new bullQueueMetrics({
+  promClient, // optional, it will use internal prom client if it is not given
+  interval: 7000, // optional, in ms, default to 60000
+}, assessmentQueueName, assessmentPrefix)
+
+// start monitoring captureQueue
+captureMetric.start(captureQueue)
+
+// start monitoring assesssmentQueue
+assessmentMetric.start(assessmentQueue)
+
+// open the url to prometheus for access ot metrics
+app.get('/metrics', function (req, res) {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(promClient.register.metrics())
+})
+*/
+app.listen(port, () => console.log(`Bull Queue Metrics listening on PORT ${port}`))
+
+process.on('SIGTERM',  () => {
+  console.info('SIGTERM signal received.')
+
+  //delay(100)
+
+  //process.exit(0)
+})
